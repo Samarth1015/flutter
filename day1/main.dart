77ade051c7b1ea47b1f1f7e644643342393a6806
samarth <202351125@iiitvadodara.ac.in>
2024-08-28 00:31:28 +0530

    oops concept

--- day1/main.dart
+++ day1/main.dart
@@ -1,3 +1,24 @@
 void main() {
   print("hello");
+  premAcc user = premAcc("samarth");
+  user.printing();
+  user.printing1();
+}
+
+class openAcc {
+  int order = 2;
+  String name = "";
+  openAcc(String name) {
+    print("hello");
+  }
+  void printing() {
+    print("hello from printing");
+  }
+}
+
+class premAcc extends openAcc {
+  premAcc(String name) : super(name);
+  void printing1() {
+    print("hello from printing 1");
+  }
 }
